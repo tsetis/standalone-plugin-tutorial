2a1a526438550429c0a1543bd6738774c7ff7545
tsetisstelios <steliost@o44.gr>
2024-06-03 20:52:15 +0300

    Steps 2, 3 & 4 done
    I did the aforementioned steps. However I am notified of an error regarding
    an undeclared variable. I will deal with it later on.

--- src/StandalonePluginTutorial.php
+++ src/StandalonePluginTutorial.php
@@ -2,6 +2,64 @@
 
 namespace Tsetis\StandalonePluginTutorial;
 
-class StandalonePluginTutorial
+use Closure;
+use Filament\Context\Concerns\HasColors;
+use Filament\Forms\Components\Component;
+use Filament\Support\Color;
+
+class StandalonePluginTutorial extends Component
 {
+    use HasColors;
+
+    protected string | int $level = 2;
+
+    protected string | Closure $content = '';
+
+    protected string $view = 'standalone-plugin-tutorial::heading';
+
+    final public function __construct(string | int $level)
+    {
+        $this->level($level);
+    }
+
+    public static function make(string | int $level): static
+    {
+        return app(static::class, ['level' => $level]);
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->dehydrated(false);
+    }
+
+    public function content(string | Closure $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function level(string | int $level): static
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    public function getColor(): array
+    {
+        return $this->evaluate($this->color) ?? Color::Amber;
+    }
+
+    public function getContent(): string
+    {
+        return $this->evaluate($this->content);
+    }
+
+    public function getLevel(): string
+    {
+        return is_int($this->level) ? 'h' . $this->level : $this->level;
+    }
 }
--- src/StandalonePluginTutorialServiceProvider.php
+++ src/StandalonePluginTutorialServiceProvider.php
@@ -2,19 +2,10 @@
 
 namespace Tsetis\StandalonePluginTutorial;
 
-use Filament\Support\Assets\AlpineComponent;
-use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
-use Filament\Support\Assets\Js;
 use Filament\Support\Facades\FilamentAsset;
-use Filament\Support\Facades\FilamentIcon;
-use Illuminate\Filesystem\Filesystem;
-use Livewire\Features\SupportTesting\Testable;
-use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Tsetis\StandalonePluginTutorial\Commands\StandalonePluginTutorialCommand;
-use Tsetis\StandalonePluginTutorial\Testing\TestsStandalonePluginTutorial;
 
 class StandalonePluginTutorialServiceProvider extends PackageServiceProvider
 {
@@ -30,125 +21,17 @@ class StandalonePluginTutorialServiceProvider extends PackageServiceProvider
          * More info: https://github.com/spatie/laravel-package-tools
          */
         $package->name(static::$name)
-            ->hasCommands($this->getCommands())
-            ->hasInstallCommand(function (InstallCommand $command) {
-                $command
-                    ->publishConfigFile()
-                    ->publishMigrations()
-                    ->askToRunMigrations()
-                    ->askToStarRepoOnGitHub('tsetis/standalone-plugin-tutorial');
-            });
-
-        $configFileName = $package->shortName();
-
-        if (file_exists($package->basePath("/../config/{$configFileName}.php"))) {
-            $package->hasConfigFile();
-        }
-
-        if (file_exists($package->basePath('/../database/migrations'))) {
-            $package->hasMigrations($this->getMigrations());
-        }
-
-        if (file_exists($package->basePath('/../resources/lang'))) {
-            $package->hasTranslations();
-        }
-
-        if (file_exists($package->basePath('/../resources/views'))) {
-            $package->hasViews(static::$viewNamespace);
-        }
+            ->hasViews();
     }
 
     public function packageRegistered(): void
     {
     }
-
     public function packageBooted(): void
     {
         // Asset Registration
         FilamentAsset::register(
-            $this->getAssets(),
-            $this->getAssetPackageName()
+            Css::make('heading', __DIR__ . '/../resources/dist/heading.css'),
         );
-
-        FilamentAsset::registerScriptData(
-            $this->getScriptData(),
-            $this->getAssetPackageName()
-        );
-
-        // Icon Registration
-        FilamentIcon::register($this->getIcons());
-
-        // Handle Stubs
-        if (app()->runningInConsole()) {
-            foreach (app(Filesystem::class)->files(__DIR__ . '/../stubs/') as $file) {
-                $this->publishes([
-                    $file->getRealPath() => base_path("stubs/standalone-plugin-tutorial/{$file->getFilename()}"),
-                ], 'standalone-plugin-tutorial-stubs');
-            }
-        }
-
-        // Testing
-        Testable::mixin(new TestsStandalonePluginTutorial());
-    }
-
-    protected function getAssetPackageName(): ?string
-    {
-        return 'tsetis/standalone-plugin-tutorial';
-    }
-
-    /**
-     * @return array<Asset>
-     */
-    protected function getAssets(): array
-    {
-        return [
-            // AlpineComponent::make('standalone-plugin-tutorial', __DIR__ . '/../resources/dist/components/standalone-plugin-tutorial.js'),
-            Css::make('standalone-plugin-tutorial-styles', __DIR__ . '/../resources/dist/standalone-plugin-tutorial.css'),
-            Js::make('standalone-plugin-tutorial-scripts', __DIR__ . '/../resources/dist/standalone-plugin-tutorial.js'),
-        ];
-    }
-
-    /**
-     * @return array<class-string>
-     */
-    protected function getCommands(): array
-    {
-        return [
-            StandalonePluginTutorialCommand::class,
-        ];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getIcons(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getRoutes(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string, mixed>
-     */
-    protected function getScriptData(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getMigrations(): array
-    {
-        return [
-            'create_standalone-plugin-tutorial_table',
-        ];
     }
 }
